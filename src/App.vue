c0da91a170d7ad4d62e4d80353d75b81f98b00c3
danifaso02 <danifaso02@gmail.com>
2022-12-01 15:26:32 +0100

    First

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <script>
-import PageTitle from './components/PageTitle.vue';
+import AppHeader from './components/AppHeader.vue';
 
 export default {
   components: {
-    PageTitle
+    AppHeader,
   }
-}
+};
 </script>
 
 <template>
   <div>
-    <PageTitle/>
+    <AppHeader />
   </div>
 </template>
 
 <style scoped>
 </style>
+
+<!-- npm install flag-icons
+
+
+@import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+<span :class="`fi fi-${show.original_language}`"></span>
+
+if (string === "en") {
+  return "gb";
+} else if (string === "ko") {
+  return "kr";
+} else return string;
+
+overflow scroll -->
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <div>
+    <input type="text">
+    <button>Invio</button>
+  </div>
+</template>
+
+<style scoped></style>
+
+
