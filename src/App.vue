399df32493bcb348b78bfdec90527dbd03988e73
danifaso02 <danifaso02@gmail.com>
2022-12-02 15:49:52 +0100

    axios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <!-- SCRIPT -->
 <script>
 import AppHeader from './components/AppHeader.vue';
+import { store } from './components/store';
 
 export default {
+  name: "App",
   components: {
-    name: "App",
     AppHeader,
-  }
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getResults() {
+      axios.get("https://api.themoviedb.org/3/movie/550?api_key=28f09a904a6b5c94bd677ec078cb8a67", {
+        params: {
+          api_key:'28f09a904a6b5c94bd677ec078cb8a67',
+          query: store.textToSearch,
+          lenguage:"it-IT",
+        },
+      })
+      .then((response) => {
+        this.store.movies = response.data.results;
+      });
+    },
+  },
 };
 </script>
 
